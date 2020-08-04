2485bf364f5fc5f9659c6db2d04d43cc90787b9e
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-04 23:31:44 +0530

    [Siva]Added:To show number of times heads and tails has won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,25 @@
 #!/bin/bash -x
 
+isTails=0
 isHeads=1
-randomCheck=$((RANDOM%2))
 
-if [ $randomCheck -eq $isHeads ]
-then
-        echo "Heads"
-else
-        echo "Trails"
-fi
+heads=0
+tails=0
+
+for((i=1; i<=10; i++))
+do
+
+	randomCheck=$((RANDOM%2))
+
+	if [ $randomCheck -eq $isHeads ]
+	then
+        	heads=$((heads+1))
+	elif [ $randomCheck -eq $isTails ]
+	then
+        	tails=$((tails+1))
+	fi
+done
+
+echo "Number of heads= " $heads
+echo "Number of tails= " $tails
 
